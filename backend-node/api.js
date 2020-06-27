4f7f4fa1d8d06039e090021985bf46e373be6c50
tkhar <themisharis@gmail.com>
2020-06-27 16:46:56 -0700

    simplified routes. postman request to multer works

--- backend-node/api.js
+++ backend-node/api.js
@@ -14,8 +14,40 @@ app.use(bodyParser.json());
 // authentication
 
 // post request to upload an image
-uploadImage = require('./routes/uploadImage');
-app.use('/', uploadImage);
+
+// multer
+const multer = require('multer');
+var upload = multer({dest: 'image-upload/'});
+
+// axios
+const axios = require('axios').default;
+let subscriptionKey = process.env['FACE_SUBSCRIPTION_KEY'];
+let endpoint = process.env['FACE_ENDPOINT'] + '/face/v1.0/detect';
+
+app.post('/image-upload', upload.single('face-image.jpg'), (req, res, next) => {
+    console.log("About to send POST request to AXIOS!")
+    let imageUrl = 'http://localhost:3000/image-uploads/upload/face-image.jpg';
+    axios({
+        method: 'post',
+        url: endpoint,
+        params: {
+            returnFaceId: true,
+            returnFaceLandmarks: false,
+            returnFaceAttributes: 'emotion'
+        },
+        data: {
+            url: imageUrl
+        }
+    }).then( function(response) {
+        console.log('Status text: ' + response.status);
+        console.log('Status text: ' + response.statusText);
+        response.data.forEach((face) => {
+            console.log('Emotion: ' + JSON.stringify(face.faceAttributes.emotion));
+        });
+    }).catch(function (error) {
+        console.log(error);
+    }); 
+});
 
 // server listens in some port
 // if port 300 is not available, try setting port to something else
@@ -26,4 +58,4 @@ app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), () => {
     console.log(`Listening on port ${app.get('port')}`);
     console.log(`Environment is ${env}`);
-})
+});
