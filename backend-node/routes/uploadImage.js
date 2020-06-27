bbd0ca5e7598880e24e1442d5b45ee1544e54d1d
hchittanuru3 <hemanthc98@outlook.com>
2020-06-27 17:22:41 -0500

    Started azure

--- backend-node/routes/uploadImage.js
+++ backend-node/routes/uploadImage.js
@@ -4,6 +4,8 @@
 
 // basic machinery
 const express = require('express');
+const http = require('http')
+const querystring = require('querystring')
 var router = express.Router();
 
 const multer = require('multer');
@@ -12,6 +14,21 @@ var upload = multer({dest: 'uploads/'});
 router.post('/image-upload', upload.single('student-image'), (req, res, next) => {
     // req.file is the image file
     // TODO: Connect to the Azure API
+    var face_api_key = process.env.FACE_KEY
+    var face_endpoint = provess.env.FACE_ENDPOINT 
+    const params = {
+        "returnFaceAttributes" : "emotion"
+    }
+    const options = {
+        url : face_endpoint,
+        method : "POST",
+        headers : {
+            'Content-Type' : 'application/json',
+            'Ocp-Apim-Subscription-Key' : face_api_key
+        }
+    }
+    // Need to add to the request body
+    
 });
 
 module.exports = router;
