ef64c49b573d807aaa34f9804f6dd5a9c41857ea
tkhar <themisharis@gmail.com>
2020-06-27 17:41:08 -0700

    Made simpler teacher route.

--- backend-node/api.js
+++ backend-node/api.js
@@ -11,12 +11,15 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-// authentication
+// Stack of emotions
+let emotions = [];
 
 // post request to upload an image
+// >>   made by student
 
 // multer
 const multer = require('multer');
+const { json } = require('express');
 var upload = multer({dest: 'image-upload/'});
 
 // axios
@@ -24,9 +27,10 @@ const axios = require('axios').default;
 let subscriptionKey = process.env['FACE_SUBSCRIPTION_KEY'];
 let endpoint = process.env['FACE_ENDPOINT'] + '/face/v1.0/detect';
 
-app.post('/image-upload', upload.single('face-image.jpg'), (req, res, next) => {
+app.post('/image-upload', upload.single('image.png'), (req, res, next) => {
+    console.log("Uploaded a file: " + req.file);
     console.log("About to send POST request to AXIOS!")
-    let imageUrl = 'http://localhost:3000/image-uploads/upload/face-image.jpg';
+    let imageUrl = 'http://localhost:3000/image-upload/image.png';
     axios({
         method: 'post',
         url: endpoint,
@@ -43,11 +47,21 @@ app.post('/image-upload', upload.single('face-image.jpg'), (req, res, next) => {
         console.log('Status text: ' + response.statusText);
         response.data.forEach((face) => {
             console.log('Emotion: ' + JSON.stringify(face.faceAttributes.emotion));
+            emotions.push(JSON.stringify(face.faceAttributes.emotion));
         });
     }).catch(function (error) {
         console.log(error);
-    }); 
+    });
 });
+
+// get request to get the current emotion
+//      >> made by teacher
+app.get('/teacher/emotion', (req, res, next) => {
+    // TODO: Send the most recent emotion from the stack of emotions!
+    const most_recent_emotion = emotions.pop();
+    res.send(`{emotion: ${most_recent_emotion}}`);
+});
+
 
 // server listens in some port
 // if port 300 is not available, try setting port to something else
