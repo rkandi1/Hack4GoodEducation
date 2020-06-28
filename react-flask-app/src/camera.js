72352408ee68405afc523d8e418450bbbfa42fa7
hchittanuru3 <hemanthc98@outlook.com>
2020-06-28 01:18:04 -0500

    Camera V1

--- react-flask-app/src/camera.js
+++ react-flask-app/src/camera.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import Webcam from "react-webcam";
 import './camera.css';
+const axios = require('axios').default;
 
 const WebcamCapture = () => {
     const webcamRef = React.useRef(null);
@@ -10,6 +11,23 @@ const WebcamCapture = () => {
   
     const capture = React.useCallback(() => {
       const imageSrc = webcamRef.current.getScreenshot();
+      var arr = imageSrc.split(',');
+      var mime = arr[0].match(/:(.*?);/)[1];
+      var bstr = atob(arr[1]);
+      var n = bstr.length;
+      var u8arr = new Uint8Array(n);
+      while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+      }
+      var the_file = new File([u8arr], "image.png", {type:mime})
+      let formData = new FormData()
+      formData.append('image.png', the_file)
+      axios({
+        method : "post",
+        url: "http://localhost:3000/image-upload",
+        data: formData,
+        headers: {'Content-Type': 'multipart/form-data' }
+      })
       setImgSrc(imageSrc);
     }, [webcamRef, setImgSrc]);
   
